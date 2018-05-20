e45846296f62d950301ced4857e52873640cb739
kamalTechTool <skamaltechtools@gmail.com>
2018-05-20 13:57:59 +0530

    First Commit - uploaded files.

---
+++ TestGithub/src/main/java/Github/TestGithub/App.java
@@ -0,0 +1,13 @@
+package Github.TestGithub;
+
+/**
+ * Testing Github Repositories!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Testing Github Program" );
+    }
+}
